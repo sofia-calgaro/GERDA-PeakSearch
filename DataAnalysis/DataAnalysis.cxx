8b9e276b462fd3049ab92b505c93fa0265574a94
sofia-calgaro <77326044+sofia-calgaro@users.noreply.github.com>
2021-04-08 12:06:53 +0200

    Add files via upload

--- DataAnalysis/DataAnalysis.cxx
+++ DataAnalysis/DataAnalysis.cxx
@@ -1,9 +1,13 @@
 #include "DataAnalysis.h"
 #include <TMath.h>
 
+
+
 bool find_max(int a, int b); // returns 'true' only if b > a
 double find_sigma(int energy); // calculates the energetic resolution for a given energy value
 double find_FWHM(int energy); // calculates the FWHM for a given energy value
+
+
 
 // ----------------------------------------------------------------------------------------------------- CONSTRUCTOR
 DataAnalysis::DataAnalysis(const std::string& name, const std::vector<unsigned int> *bin_content, int E0)
@@ -14,35 +18,40 @@ DataAnalysis::DataAnalysis(const std::string& name, const std::vector<unsigned i
             int x2 = E0+12;
                             
             double FWHM = find_FWHM(E0);
-           
-            int x1_stop = std::round( E0 - 1.5*FWHM );
-            int x2_start = std::round( E0 + 1.5*FWHM );
-                   
+            
+            // Let's define the range [x1_sig;x2_sig] in which the signal contribution (S) is calculated
+            int x1_sig = std::round( E0 - 1.5*FWHM );
+            int x2_sig = std::round( E0 + 1.5*FWHM );
+            
+            // Calculate the average BKG outside [x1_sig;x2_sig]   
             int B_i = 0;
                     
-            for ( int i=x1+1; i<=x1_stop; i++ ) { B_i += bin_content->at(i); }
+            for ( int i=x1+1; i<=x1_sig; i++ ) { B_i += bin_content->at(i); }
             
-            for ( int i=x2_start+1; i<=x2; i++ ) { B_i += bin_content->at(i); }
+            for ( int i=x2_sig+1; i<=x2; i++ ) { B_i += bin_content->at(i); }
                     
-            int N_bkg = ( x1_stop - x1 ) + ( x2 - x2_start );
+            int N_bkg = ( x1_sig - x1 ) + ( x2 - x2_sig );
             int B_avg = std::round( B_i / N_bkg );
                     
+            // Calculate the overall bin content in [x1:sig;x2_sig]        
             int S_i = 0;
-            for ( int i=x1_stop+1; i<=x2_start; i++ ) { S_i += bin_content->at(i); }
+            for ( int i=x1_sig+1; i<=x2_sig; i++ ) { S_i += bin_content->at(i); }
                     
-            int N_sig = x2_start-x1_stop;
-                    
+            int N_sig = x2_sig-x1_sig;
+            
+            // Calculate the amount of BKG in [x1:sig;x2_sig]        
             int B_sig = N_sig*B_avg;
-                    
+            
+            // Calculate the signal in [x1:sig;x2_sig]      
             int S = std::round ( S_i-B_sig );                
                     
+            // Find the maximum value that the signal height can assume        
             int max1 = std::round( S + 5*sqrt(B_sig) + 5*sqrt(S) );
             int max2 = std::round( 8*sqrt(B_sig) );
             int max3 = 10;
             int max = std::max({max1, max2, max3},find_max);
         
 
-            // Let's add parameters/priors for a Gaussian+pol0 function:
             // 1) Signal yield (index 0)
             AddParameter("height", 0, max, "", "[events]");
             GetParameters().Back().SetPriorConstant();
@@ -52,8 +61,12 @@ DataAnalysis::DataAnalysis(const std::string& name, const std::vector<unsigned i
             GetParameters().Back().SetPriorConstant();
 }
 
+
+
 // ----------------------------------------------------------------------------------------------------- DESTRUCTOR
 DataAnalysis::~DataAnalysis() { }
+
+
 
 // ----------------------------------------------------------------------------------------------------- MY MODEL
 double DataAnalysis::LogLikelihood(const std::vector<double>& pars)
